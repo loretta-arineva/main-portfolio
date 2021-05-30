16cd427e65c0166ac0e0f70a00a28a307e24e211
loretta-arineva <wowshifter@gmail.com>
2021-05-30 20:58:44 +0200

    Added link on main logo

--- src/pages/components/multifunctional/Logo.js
+++ src/pages/components/multifunctional/Logo.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
     return (
         <div className="logo--container">
-            <p>Loreta</p>
-            <p>Krasteva</p>
+            <Link to="/">
+                <p>Loreta</p>
+                <p>Krasteva</p>
+            </Link>
         </div>
     )
 }
