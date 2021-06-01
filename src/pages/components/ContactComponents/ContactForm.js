61eb1941275b885305af06ebedaf3aad970d48c3
loretta-arineva <wowshifter@gmail.com>
2021-06-01 16:47:59 +0800

    Fixed warnings

--- src/pages/components/ContactComponents/ContactForm.js
+++ src/pages/components/ContactComponents/ContactForm.js
@@ -14,9 +14,9 @@ const ContactForm = () => {
 
     return (
         <form action="https://formsubmit.io/send/b5058b82-60cd-4991-ad75-b5678bcdbbbe" method="POST" id="contact-form" onSubmit={formSubmitHandler} target="_blank">
-            <input type="text" maxlength="256" placeholder="Your Full Name" id="full-name" name="name" required ref={nameRef}  />
-            <input type="email" maxlength="256" placeholder="Your Email" id="email" required name="email" ref={emailRef} />
-            <textarea name="" id="" cols="20" rows="10" maxlength="5000" placeholder="Your Message..." required name="text" ref={messageRef} ></textarea>
+            <input type="text" maxLength="256" placeholder="Your Full Name" id="full-name" name="name" required ref={nameRef}  />
+            <input type="email" maxLength="256" placeholder="Your Email" id="email" required name="email" ref={emailRef} />
+            <textarea id="" cols="20" rows="10" maxLength="5000" placeholder="Your Message..." required name="text" ref={messageRef} ></textarea>
             {/* Spam prevention */}
             <input name="_formsubmit_id" type="text" style={{display: "none"}}></input>
             <input type="submit" value="Submit" className="form-submit__btn" />
