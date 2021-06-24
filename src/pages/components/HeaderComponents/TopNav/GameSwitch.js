2969fca4cec5f1ef7a32d69277ae4ef02d42ff34
loretta-arineva <wowshifter@gmail.com>
2021-06-24 14:38:36 +0100

    Initial game feature commit

--- src/pages/components/HeaderComponents/TopNav/GameSwitch.js
+++ src/pages/components/HeaderComponents/TopNav/GameSwitch.js
@@ -6,10 +6,9 @@ const GameSwitch = () => {
     return (
         <div className="game-switch--container">
             <div className="ct">
-
-            <div id='warped'>
-                <span className='w0'>P</span><span className='w1'>L</span><span className='w2'>A</span><span className='w3'>Y</span><span className='w4'> </span><span className='w5'>G</span><span className='w6'>A</span><span className='w7'>M</span><span className='w8'>E</span>
-            </div>
+                <div id='warped'>
+                    <span className='w0'>P</span><span className='w1'>L</span><span className='w2'>A</span><span className='w3'>Y</span><span className='w4'> </span><span className='w5'>G</span><span className='w6'>A</span><span className='w7'>M</span><span className='w8'>E</span>
+                </div>
             </div>
             <div className="play-btn">
                 <FontAwesomeIcon icon={faPlay} />
