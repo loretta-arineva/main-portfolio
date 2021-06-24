22a8fde7370a7a8b5980d556ef0cb538a6d52906
loretta-arineva <wowshifter@gmail.com>
2021-06-24 14:43:02 +0100

    Refactored routes

--- src/pages/containers/App.js
+++ src/pages/containers/App.js
@@ -43,15 +43,21 @@ function App() {
         <HeaderNav />
         <ProgressBar scroll={scroll} />
         <Switch>
+          <Route path="/" exact>
+            <Hero />
+          </Route>
+
           <Route path="/about">
             <About />
           </Route>
+
           <Route path="/contact">
             <Contact />
           </Route>
+
           <Route path="/">
-            <Hero />
           </Route>
+
         </Switch>
         <Footer />
       </Router>
