791c980d978ea51b6b0d2102568e92593083518c
loretta-arineva <wowshifter@gmail.com>
2021-06-24 17:47:31 +0100

    Fixed contact form

--- src/pages/components/ContactComponents/ContactForm.js
+++ src/pages/components/ContactComponents/ContactForm.js
@@ -6,19 +6,13 @@ const ContactForm = () => {
     const emailRef = useRef();
     const messageRef = useRef();
 
-    const formSubmitHandler = (event) => {
-        nameRef.current.value = '';
-        emailRef.current.value = '';
-        messageRef.current.value = '';
-    }
-
     return (
-        <form action="https://formsubmit.io/send/b5058b82-60cd-4991-ad75-b5678bcdbbbe" method="POST" id="contact-form" onSubmit={formSubmitHandler} target="_blank">
-            <input type="text" maxLength="256" placeholder="Your Full Name" id="full-name" name="name" required ref={nameRef}  />
+        <form action="https://formsubmit.io/send/b5058b82-60cd-4991-ad75-b5678bcdbbbe" method="POST" id="contact-form" target="_blank">
+            <input type="text" maxLength="256" placeholder="Your Full Name" id="full-name" name="name" required ref={nameRef} />
             <input type="email" maxLength="256" placeholder="Your Email" id="email" required name="email" ref={emailRef} />
             <textarea id="" cols="20" rows="10" maxLength="5000" placeholder="Your Message..." required name="text" ref={messageRef} ></textarea>
             {/* Spam prevention */}
-            <input name="_formsubmit_id" type="text" style={{display: "none"}}></input>
+            {/* <input name="_formsubmit_id" type="text" style={{display: "none"}}></input> */}
             <input type="submit" value="Submit" className="form-submit__btn" />
         </form>
     )
