4ae4c26b35e382614084d60fc7f935e113ad094c
loretta-arineva <wowshifter@gmail.com>
2021-06-24 14:31:35 +0100

    Added scroll bar functionality

--- src/pages/components/HeaderComponents/HeaderNav/ProgressBar/ProgressBar.js
+++ src/pages/components/HeaderComponents/HeaderNav/ProgressBar/ProgressBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const ProgressBar = () => {
+const ProgressBar = ({ scroll }) => {
+    const width = (scroll * 100).toFixed();
+    console.log(width);
+    
     return (
-        <div className="progress-bar--container">
-            <div className="progress-bar"></div>
+        <div className="progress-bar__container">
+            <div className="progress-bar" style={{ width: `${width}%` }}></div>
         </div>
     )
 }
--- src/pages/containers/App.js
+++ src/pages/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
 import {
   BrowserRouter as Router,
@@ -17,12 +17,31 @@ const About = React.lazy(() => import('./About'));
 const Contact = React.lazy(() => import('./Contact'));
 
 function App() {
+  // Scroll bar
+  const [scroll, setScroll] = useState(0);
+
+  useEffect(() => {
+
+    let progressBarHandler = () => {
+
+      const totalScroll = document.documentElement.scrollTop;
+      const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scroll = `${totalScroll / windowHeight}`;
+
+      setScroll(scroll);
+    }
+
+    window.addEventListener("scroll", progressBarHandler);
+
+    return () => window.removeEventListener("scroll", progressBarHandler);
+  });
+
   return (
     <Suspense fallback={<p>Loading...</p>}>
       <Router>
         <Header />
         <HeaderNav />
-        <ProgressBar />
+        <ProgressBar scroll={scroll} />
         <Switch>
           <Route path="/about">
             <About />
