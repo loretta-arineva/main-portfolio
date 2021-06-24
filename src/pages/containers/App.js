bc7c8c46fe7280949956dd52ee3613b23f9205d4
loretta-arineva <wowshifter@gmail.com>
2021-06-24 10:55:35 +0100

    Added lazy loading

--- src/pages/containers/App.js
+++ src/pages/containers/App.js
@@ -1,37 +1,42 @@
+import React, { Suspense } from 'react';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
 
-import Header from './Header';
 import '../../assets/scss/main.scss';
 import HeaderNav from '../components/HeaderComponents/HeaderNav/RightNav/HeaderNav';
 import ProgressBar from '../components/HeaderComponents/HeaderNav/ProgressBar/ProgressBar';
-import Hero from './Hero';
-import About from './About';
-import Contact from './Contact';
-import Footer from './Footer';
+
+const Header = React.lazy(() => import('./Header'));
+const Footer = React.lazy(() => import('./Footer'));
+const Hero = React.lazy(() => import('./Hero'));
+const About = React.lazy(() => import('./About'));
+const Contact = React.lazy(() => import('./Contact'));
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <HeaderNav />
-      <ProgressBar />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/">
-          <Hero />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Router>
+        <Header />
+        <HeaderNav />
+        <ProgressBar />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Hero />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </Suspense>
   );
 }
 
